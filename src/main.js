d00abf6d2cf8742ae0842569988bb40858d0ef42
Oriane <oriane.louis.pro@gmail.com>
2020-12-28 18:59:48 -0500

    chore: Add vue-router to the Vue config

--- src/main.js
+++ src/main.js
@@ -1,8 +1,18 @@
-import Vue from 'vue'
-import App from './App.vue'
+import Vue from "vue";
+import App from "./App.vue";
+import VueRouter from "vue-router";
 
-Vue.config.productionTip = false
+import routes from "./routes/index.js";
+
+Vue.use(VueRouter);
+Vue.config.productionTip = false;
+
+var router = new VueRouter({
+  routes,
+  mode: "history",
+});
 
 new Vue({
-  render: h => h(App),
-}).$mount('#app')
+  router,
+  render: (h) => h(App),
+}).$mount("#app");
