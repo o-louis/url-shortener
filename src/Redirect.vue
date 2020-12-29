a50af3236a26c28e311b8e4bd480f27f99bf9c10
Oriane <oriane.louis.pro@gmail.com>
2020-12-28 19:50:31 -0500

    feat: Do some check about the params url, if exist redirect to the website

--- src/Redirect.vue
+++ src/Redirect.vue
@@ -1,9 +1,31 @@
 <template>
-  <div class="text-white">REDIRECT</div>
+  <div></div>
 </template>
-
 <script>
 export default {
   name: "Redirect",
+  props: {
+    listUrls: Array,
+  },
+  created() {
+    let listUrls = JSON.parse(localStorage.getItem("list-urls")) || [];
+    let requestedUrl = this.$route.params?.url_shortened;
+    if (requestedUrl) {
+      const url = listUrls.find((item) => item.id === requestedUrl);
+      if (url?.name) {
+        window.location.href = this.addhttp(url.name);
+      } else {
+        this.$router.push("/");
+      }
+    }
+  },
+  methods: {
+    addhttp(url) {
+      if (!/^(?:f|ht)tps?:\/\//.test(url)) {
+        url = "http://" + url;
+      }
+      return url;
+    },
+  },
 };
 </script>
